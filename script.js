c15675c5be5fb9d65252dae1fff1968300960c1a
willymariino <wg.mariino@gmail.com>
2025-07-15 23:48:11 +0200

    snack 3 - part 1

--- script.js
+++ script.js
@@ -96,3 +96,17 @@ console.log("libri scontati:", discountedBooks)
 discountedBooks.forEach(bookPrice => {
     console.log(bookPrice.toFixed(2))
 })
+
+/*
+
+Snack 3 - Ordinare gli Autori
+Creare un array (authors) che contiene gli autori dei libri.
+Crea una variabile booleana (areAuthorsAdults) per verificare se gli autori sono tutti maggiorenni.
+Ordina l’array authors in base all’età, senza creare un nuovo array.
+(se areAuthorsAdult è true, ordina in ordine crescente, altrimenti in ordine decrescente)
+
+*/
+
+authors = books.map(A => A.author.name)
+
+console.log(authors)
