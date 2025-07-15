8f086049d1acc151143ccbb88e3a7719a22779d5
willymariino <wg.mariino@gmail.com>
2025-07-16 00:42:04 +0200

    snack 4

--- script.js
+++ script.js
@@ -118,3 +118,24 @@ console.log(areAuthorsAdults)
 authors.sort((a, b) => b.age - a.age)
 
 console.log(authors)
+
+/*
+Snack 4 - Calcola l’età media
+Creare un array (ages) che contiene le età degli autori dei libri.
+Calcola la somma delle età (agesSum) usando reduce.
+Stampa in console l’età media degli autori dei libri.
+*/
+
+ages = books.map(eta => eta.author.age)
+
+console.log(ages)
+
+const sumAges = ages.reduce((acc, curr) => {
+
+    return acc + curr
+
+}, 0)
+
+const medianAge = sumAges / ages.lenght
+
+console.log(medianAge)
